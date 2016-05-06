a223e6b0f2851e60d601bd62ae42b646dd56ab5e
Miroslav Furda <miroslav.furda@senacor.com>
2016-05-06 11:22:02 +0200

    wrapper project

--- Pinz.Client.Base/Properties/AssemblyInfo.cs
+++ Pinz.Client.Base/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Pinz.Client.DomainModel")]
+[assembly: AssemblyTitle("Pinz.Client.Base")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pinz.Client.DomainModel")]
+[assembly: AssemblyProduct("Pinz.Client.Base")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("85aedd42-798e-4d82-a2c7-53d63de77d02")]
+[assembly: Guid("a5ab85f3-32e7-4aa7-9540-4bdffee5201f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
